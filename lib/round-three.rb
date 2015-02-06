3bcca6701f386451f46d13094faac0b4aef66045
meads58 <meads109@gmail.com>
2015-02-06 14:26:48 +0000

    Added ex3

---
+++ lib/round-three.rb
@@ -0,0 +1,6 @@
+class Catswithhats
+
+def initialize
+	cat_line = Array.new(100, true)
+end
+end
--- spec/round-three_spec.rb
+++ spec/round-three_spec.rb
@@ -2,8 +2,13 @@ require('round-three')
 
 describe('cats with hats') do
 
-  it('') do
+let(:create){Catswithhats.new}
 
+  it('should create 100 cats') do
+  	expect(create.cat_line.length).to eq(100)
   end
+
+
+
 
 end
